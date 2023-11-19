d0b7401383053602540334e0ac60dee776442b78
Diego Calciolari Gordiano <diego.gordiano@acad.espm.br>
2023-11-19 01:39:26 -0300

    Ex 2 e 3

---
+++ Exercicio 3/Program.cs
@@ -0,0 +1,44 @@
+﻿using SkiaSharp;
+
+namespace Projeto
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (SKBitmap bitmapEntrada = SKBitmap.Decode("C:\\Users\\User\\Desktop\\Atv_Individual2\\Exercicio 3\\Exercicio 3.png"))
+			{
+
+				unsafe
+				{
+					byte* entrada = (byte*)bitmapEntrada.GetPixels();
+
+					int altura = bitmapEntrada.Height;
+					int largura = bitmapEntrada.Width;
+
+					int pixelsTotais = bitmapEntrada.Width * bitmapEntrada.Height;
+
+					int[] pixels = new int[256];
+
+					int pixelsContados = 0;
+
+					for (int y = 0; y < altura; y++)
+					{
+						for (int x = 0; x < largura; x++)
+						{
+							pixels[entrada[y * largura + x]] += 1;
+						}
+					}
+
+					for (int z = 0; z <= 255; z++)
+					{
+						Console.WriteLine("Quantidade de pixels com valor " + z + ": " + pixels[z]);
+						pixelsContados += pixels[z];
+					}
+					Console.WriteLine("Pixels Totais: " + pixelsTotais);
+					Console.WriteLine("Pixels contados: " + pixelsContados);
+				}
+			}
+		}
+	}
+}
